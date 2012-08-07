bf40f460c16deb0148c60a701b5d650ecbf55d27
Gerd Behrmann <behrmann@gmail.com>
2012-08-07 10:08:24 +0200

    Implement toString in response messages.

--- xrootd4j/src/main/java/org/dcache/xrootd/protocol/messages/RedirectResponse.java
+++ xrootd4j/src/main/java/org/dcache/xrootd/protocol/messages/RedirectResponse.java
@@ -27,6 +27,11 @@ public class RedirectResponse extends AbstractResponseMessage
     private final static Logger _logger =
         LoggerFactory.getLogger(RedirectResponse.class);
 
+    private final String _host;
+    private final int _port;
+    private final String _opaque;
+    private final String _token;
+
     public RedirectResponse(XrootdRequest request, String host, int port)
     {
         this(request, host, port, "", "");
@@ -36,6 +41,11 @@ public class RedirectResponse extends AbstractResponseMessage
     {
         super(request, XrootdProtocol.kXR_redirect,
               4 + host.length() + opaque.length() + token.length() + 2);
+
+        _host = host;
+        _port = port;
+        _opaque = opaque;
+        _token = token;
 
         putSignedInt(port);
         _logger.info("Sending the following host information to the client: {}", host);
@@ -54,5 +64,11 @@ public class RedirectResponse extends AbstractResponseMessage
             putCharSequence("?");
             putCharSequence(token);
         }
+    }
+
+    @Override
+    public String toString()
+    {
+        return String.format("redirect[%s:%d,%s,%s]", _host, _port, _opaque, _token);
     }
 }
