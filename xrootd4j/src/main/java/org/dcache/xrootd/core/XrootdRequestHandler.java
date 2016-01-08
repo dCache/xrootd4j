7fca05cd27dd25844e1a84065d656c314887a342
Gerd Behrmann <behrmann@gmail.com>
2016-01-08 12:20:21 +0100

    xrootd4j: Add support for dispatching kXR_endsess requests
    Target: master, 3.0, 2.1
    Acked-by: Paul Millar <paul.millar@desy.de>
    Patch: https://rb.dcache.org/r/8922/

--- xrootd4j/src/main/java/org/dcache/xrootd/core/XrootdRequestHandler.java
+++ xrootd4j/src/main/java/org/dcache/xrootd/core/XrootdRequestHandler.java
@@ -29,6 +29,7 @@ import org.slf4j.LoggerFactory;
 import org.dcache.xrootd.protocol.messages.AuthenticationRequest;
 import org.dcache.xrootd.protocol.messages.CloseRequest;
 import org.dcache.xrootd.protocol.messages.DirListRequest;
+import org.dcache.xrootd.protocol.messages.EndSessionRequest;
 import org.dcache.xrootd.protocol.messages.ErrorResponse;
 import org.dcache.xrootd.protocol.messages.LocateRequest;
 import org.dcache.xrootd.protocol.messages.LoginRequest;
@@ -164,6 +165,10 @@ public class XrootdRequestHandler extends ChannelInboundHandlerAdapter
             case kXR_set :
                 response =
                         doOnSet(ctx, (SetRequest) req);
+                break;
+            case kXR_endsess:
+                response =
+                        doOnEndSession(ctx, (EndSessionRequest) req);
                 break;
             default:
                 response =
@@ -345,6 +350,13 @@ public class XrootdRequestHandler extends ChannelInboundHandlerAdapter
 
     protected Object doOnSet(ChannelHandlerContext ctx,
                              SetRequest request)
+            throws XrootdException
+    {
+        return unsupported(ctx, request);
+    }
+
+    protected Object doOnEndSession(ChannelHandlerContext ctx,
+                                    EndSessionRequest request)
             throws XrootdException
     {
         return unsupported(ctx, request);
