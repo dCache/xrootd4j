858042cc6b34277c0b579fc22dc21103fd0aeb75
Dmitry Litvintsev <litvinse@fnal.gov>
2018-10-04 15:16:38 -0500

    DHession: do not pad byte array with 0 when generating SecretKey           on decrypt and encrypt
    Motivation:
    
    In about 0.5% of cases we see handshake failures caused by failure to
    decrypt client data due to xrdcp using non padded version of DH_compute_key
    
    The issue was introduced when we switched to newer BC 1.50, commit
    c68b78162c6a96f8d7bd5f082a7bf2d09ed8785b.
    
    Modification:
    
    When decrypting use use/try catch block to retry on the
    first BadPaddingException after using KeyAgreement.generateSecret()
    key and then use KeyAgreement.generateSecret("TlsPremasterSecret")
    and retry. Patch is structured so that new code path will be executed in
    0.5% of cases on a failure only, to minimize impact (if any).
    
    When encrypting use KeyAgreement.generateSecret("TlsPremasterSecret")
    for compatibility with SLAC xrootd sever.
    
    Result:
    
    No longer see failures due to 'pad block corrupted' problem if using
    modifed BC jar that handles "TlsPremasterSecret" as special case.
    
    When used against vanilla BC 1.50 jar the error still occurs. When
    we switch to more modern version of BC it will work out of the box
    (the fix is implemented in 1.51 and higher).
    
    Ticket: http://rt.dcache.org/Ticket/Display.html?id=9129
    Ticket: http://rt.dcache.org/Ticket/Display.html?id=9218
    Ticket: http://rt.dcache.org/Ticket/Display.html?id=9444
    Issue: https://github.com/dCache/xrootd4j/issues/6.
    
    Pull Request: https://github.com/dCache/xrootd4j/pull/16
    
    RB : https://rb.dcache.org/r/11225/
    Acked-by: Al Rossi <arossi@fnal.gov>
    
    Target: trunk
    Request:
    (cherry picked from commit 7b85075fe281cb189c6a12139c6b031bf83124ae)
    Signed-off-by: Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>

--- xrootd4j-gsi/src/main/java/org/dcache/xrootd/plugins/authn/gsi/DHSession.java
+++ xrootd4j-gsi/src/main/java/org/dcache/xrootd/plugins/authn/gsi/DHSession.java
@@ -189,11 +189,38 @@ public class DHSession
                BadPaddingException, InvalidAlgorithmParameterException,
                NoSuchProviderException
     {
-        return translate(cipherSpec,
-                         keySpec,
-                         blocksize,
-                         encrypted,
-                         Cipher.DECRYPT_MODE);
+        byte [] iv = new byte[blocksize];
+        Arrays.fill(iv, (byte)0);
+        IvParameterSpec paramSpec = new IvParameterSpec(iv);
+        Cipher cipher = Cipher.getInstance(cipherSpec,
+                                           "BC");
+        byte[] decrypted;
+        SecretKey sessionKey;
+        /**
+         * This try / catch construct is used to handle
+         * xrootd clients that do not use padded version of DH_compute_key
+         * Once they switched to DH_compute_key_padded this will no longer
+         * be neeeded.
+         */
+        try {
+            /* need a 128-bit key, that's the way to get it */
+            sessionKey = new SecretKeySpec(_keyAgreement.generateSecret(),
+                                           0,
+                                           blocksize,
+                                           keySpec);
+            cipher.init(Cipher.DECRYPT_MODE, sessionKey, paramSpec);
+            decrypted = cipher.doFinal(encrypted);
+        } catch (BadPaddingException e) {
+            sessionKey = new SecretKeySpec(_keyAgreement
+                                           .generateSecret("TlsPremasterSecret")
+                                           .getEncoded(),
+                                           0,
+                                           blocksize,
+                                           keySpec);
+            cipher.init(Cipher.DECRYPT_MODE, sessionKey, paramSpec);
+            decrypted = cipher.doFinal(encrypted);
+        }
+        return decrypted;
     }
 
     public byte[] encrypt(String cipherSpec,
@@ -206,39 +233,24 @@ public class DHSession
                     BadPaddingException, InvalidAlgorithmParameterException,
                     NoSuchProviderException
     {
-        return translate(cipherSpec,
-                         keySpec,
-                         blocksize,
-                         unencrypted,
-                         Cipher.ENCRYPT_MODE);
-    }
-
-    private byte[] translate(String cipherSpec,
-                             String keySpec,
-                             int blocksize,
-                             byte[] buffer,
-                             int mode)
-                    throws InvalidKeyException,
-                    IllegalStateException, NoSuchAlgorithmException,
-                    NoSuchPaddingException, IllegalBlockSizeException,
-                    BadPaddingException, InvalidAlgorithmParameterException,
-                    NoSuchProviderException
-    {
         byte [] iv = new byte[blocksize];
         Arrays.fill(iv, (byte)0);
-        final byte[] sharedSecret = _keyAgreement.generateSecret();
-        /* need a 128-bit key, that's the way to get it */
-        SecretKey sessionKey = new SecretKeySpec(sharedSecret,
+        IvParameterSpec paramSpec = new IvParameterSpec(iv);
+        Cipher cipher = Cipher.getInstance(cipherSpec,
+                                           "BC");
+        /**
+         * Unlike decrypt, the encrypt method always has use
+         * .generateSecret("TlsPremasterSecret") to be compatible
+         * with SLAC server (e.g. for TPC)
+         */
+        SecretKey sessionKey = new SecretKeySpec(_keyAgreement
+                                                 .generateSecret("TlsPremasterSecret")
+                                                 .getEncoded(),
                                                  0,
                                                  blocksize,
                                                  keySpec);
-        IvParameterSpec paramSpec = new IvParameterSpec(iv);
-
-        Cipher cipher = Cipher.getInstance(cipherSpec,
-                                           "BC");
-        cipher.init(mode, sessionKey, paramSpec);
-
-        return cipher.doFinal(buffer);
+        cipher.init(Cipher.ENCRYPT_MODE, sessionKey, paramSpec);
+        return cipher.doFinal(unencrypted);
     }
 
     /**
