a9cecd3572c8b93fc8e7c56cb196f70284354ff6
larrieu <larrieu@jlab.org>
2022-08-23 12:19:34 -0400

    Added reference counting for ByteBuf.  Applied google java style.

--- xrootd4j/src/main/java/org/dcache/xrootd/protocol/messages/FattrRequest.java
+++ xrootd4j/src/main/java/org/dcache/xrootd/protocol/messages/FattrRequest.java
@@ -8,10 +8,10 @@
  * the License, or (at your option) any later version.
  *
  * xrootd4j is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
- * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public License along with xrootd4j.  If
+ * You should have received a copy of the GNU Lesser General Public License along with xrootd4j. If
  * not, see http://www.gnu.org/licenses/.
  */
 package org.dcache.xrootd.protocol.messages;
@@ -24,109 +24,106 @@ import static org.dcache.xrootd.protocol.XrootdProtocol.kXR_fattrList;
 import static org.dcache.xrootd.protocol.XrootdProtocol.kXR_fattrSet;
 import static org.dcache.xrootd.protocol.XrootdProtocol.kXR_fattr_aData;
 import static org.dcache.xrootd.protocol.XrootdProtocol.kXR_fattr_isNew;
-
 import java.util.Arrays;
-
 import io.netty.buffer.ByteBuf;
 
 /**
- *  Not every QueryRequest is a path request, so it
- *  does not extend that class.
+ * Not every QueryRequest is a path request, so it does not extend that class.
  */
 public class FattrRequest extends AbstractXrootdRequest {
 
-    private final int fhandle;
-    private final int subcode;
-    private final int nattr;
-    private final int options;
-    private final String[] nvec;
+  private final int fhandle;
+  private final int subcode;
+  private final int nattr;
+  private final int options;
+  private final String[] nvec;
 
-    /*
-     *  Can be altered by authorization plugins.
-     */
-    private String path;
+  /*
+   * Can be altered by authorization plugins.
+   */
+  private String path;
 
-    
-    public FattrRequest(ByteBuf buffer) {
-        super(buffer, kXR_fattr);
-        fhandle = buffer.getInt(4);
-        subcode = buffer.getUnsignedByte(8);
-    	int alen = buffer.getInt(20);
-    	
-    	ByteBuf bb = buffer.slice(24,alen);
-    	
-    	int pos = 0;
-    	int len = bb.bytesBefore(pos,alen,(byte)0);
-    	path = bb.toString(pos,len,US_ASCII);
-    	pos += len + 1;
-    	
-    	if (isList()) {
-    		nattr = 0;
-    		nvec = new String[0];
-    	}
-    	else {
-    		nattr = buffer.getUnsignedByte(9);
-    		nvec = new String[nattr];
-    		for (int i = 0; i < nattr; ++i) {
-    			pos += 2;
-    			len = bb.bytesBefore(pos,alen-pos,(byte)0);
-    			nvec[i] = bb.toString(pos,len,US_ASCII);
-    			pos += len;
-    		}
-    	}
-    	options = buffer.getUnsignedByte(10);
-    }
 
-    public int getCode() {
-        return subcode;
-    }
+  public FattrRequest(ByteBuf buffer) {
+    super(buffer, kXR_fattr);
+    fhandle = buffer.getInt(4);
+    subcode = buffer.getUnsignedByte(8);
+    int alen = buffer.getInt(20);
 
-    public int getFhandle() {
-        return fhandle;
-    }
+    ByteBuf bb = buffer.slice(24, alen);
 
-    public boolean isNew() {
-    	return (options & kXR_fattr_isNew) == kXR_fattr_isNew;
-    }
+    int pos = 0;
+    int len = bb.bytesBefore(pos, alen, (byte) 0);
+    path = bb.toString(pos, len, US_ASCII);
+    pos += len + 1;
 
-    public boolean aData() {
-    	return (options & kXR_fattr_aData) == kXR_fattr_aData;
+    if (isList()) {
+      nattr = 0;
+      nvec = new String[0];
+    } else {
+      nattr = buffer.getUnsignedByte(9);
+      nvec = new String[nattr];
+      for (int i = 0; i < nattr; ++i) {
+        pos += 2;
+        len = bb.bytesBefore(pos, alen - pos, (byte) 0);
+        nvec[i] = bb.toString(pos, len, US_ASCII);
+        pos += len;
+      }
     }
-    
-    public boolean isList() {
-    	return subcode == kXR_fattrList;
-    }
+    options = buffer.getUnsignedByte(10);
+  }
 
-    public boolean isGet() {
-    	return subcode == kXR_fattrGet;
-    }
+  public int getCode() {
+    return subcode;
+  }
 
-    public boolean isSet() {
-    	return subcode == kXR_fattrSet;
-    }
+  public int getFhandle() {
+    return fhandle;
+  }
 
-    public boolean isDel() {
-    	return subcode == kXR_fattrDel;
-    }
+  public boolean isNew() {
+    return (options & kXR_fattr_isNew) == kXR_fattr_isNew;
+  }
 
-    public String getPath() {
-        return path;
-    }
-    
-    public String[] getNames() {
-    	return nvec;
-    }
-    
-    public int getNattr() {
-    	return nattr;
-    }
+  public boolean aData() {
+    return (options & kXR_fattr_aData) == kXR_fattr_aData;
+  }
 
-    public void setPath(String path) {
-        this.path = path;
-    }
+  public boolean isList() {
+    return subcode == kXR_fattrList;
+  }
 
-    @Override
-    public String toString() {
-        return String.format("fattr[%d,%d,%d,%d,%s]", subcode, fhandle, nattr, options, Arrays.asList(nvec).toString());
-    }
+  public boolean isGet() {
+    return subcode == kXR_fattrGet;
+  }
+
+  public boolean isSet() {
+    return subcode == kXR_fattrSet;
+  }
+
+  public boolean isDel() {
+    return subcode == kXR_fattrDel;
+  }
+
+  public String getPath() {
+    return path;
+  }
+
+  public String[] getNames() {
+    return nvec;
+  }
+
+  public int getNattr() {
+    return nattr;
+  }
+
+  public void setPath(String path) {
+    this.path = path;
+  }
+
+  @Override
+  public String toString() {
+    return String.format("fattr[%d,%d,%d,%d,%s]", subcode, fhandle, nattr, options,
+        Arrays.asList(nvec).toString());
+  }
 }
